baa029a088ec77f9fa53cb52d4e93d2b263fa864
mz0702 <mz0702@users.noreply.github.com>
2026-06-15 12:54:30 +0200

    Deploy to GitHub Pages with dynamic base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build
 export default defineConfig({
-  site: 'https://www.ycat.de',
+  site: 'https://mz0702.github.io',
+  base: process.env.GITHUB_ACTIONS ? '/YCAT' : '/',
   build: { format: 'directory' },
 });
